44912fa42e6cc95b28d1c3d0222033acd0775206
Chathura Lakshan <99183326+IT21168536@users.noreply.github.com>
2022-02-16 01:51:29 +0530

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -5,35 +5,38 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
-int minimum(int n1, int n2);
-int maximum(int n1, int n2);
-int multiply(int n1, int n2);   
+int minimum(int n1, int n2);// function prototype
+int maximum(int n1, int n2);// function prototype
+int multiply(int n1, int n2); // function prototype 
 int main() {
    
    int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("min     = %d\n", minimum(no1, no2));
-   printf("max     = %d\n", maximum(no1, no2));
-   printf("multiply= %d\n", multiply(no1, no2));
+   printf("Enter a value for no 1 : ");// get value
+   scanf("%d", &no1);// read value
+   printf("Enter a value for no 2 : ");// get value
+   scanf("%d", &no2);// read value
+   printf("min     = %d\n", minimum(no1, no2));// print minimum
+   printf("max     = %d\n", maximum(no1, no2));// print maximum
+   printf("multiply= %d\n", multiply(no1, no2));// print multiply
    return 0;
 }
+//called funtion
 int minimum(int n1, int n2){
 if(n1<n2)
-   return n1;
+   return n1;// return value
 else
-   return n2;
+   return n2;// return value
 }
+//called funtion
 int maximum(int n1, int n2){
 if(n1<n2)
-   return n2;
+   return n2;// return value
 else
-   return n1;
+   return n1;// return value
 }
+//called funtion
 int multiply(int n1, int n2){
 int mult;
-mult = n1 * n2;
-   return mult;
+mult = n1 * n2;// calculate multiply
+   return mult;// return value
 }
