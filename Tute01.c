2d21242825080221aeeeeb327a2c4a1baa8d5b94
Chathura Lakshan <99183326+IT21168536@users.noreply.github.com>
2022-02-16 01:53:48 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,16 +5,16 @@
 #include <stdio.h>
 
 int main() {
-   int sub1;
-   int sub2;
-   float avg;
-   printf("enter mark1 =");
-   scanf("%d",&sub1);
-   printf("enter mark2 =");
-   scanf("%d",&sub2);
-   avg = (sub1 + sub2) / 2.00;
-   printf("Average = %.2f ",avg);
-  
+   int sub1;// declare variable
+   int sub2;// declare variable
+   float avg;// declare variable
+   
+   printf("enter mark1 =");// gert value
+   scanf("%d",&sub1);// read value
+   printf("enter mark2 =");// gert value
+   scanf("%d",&sub2);// read value
+   avg = (sub1 + sub2) / 2.00;// calculate average
+   printf("Average = %.2f ",avg);// print average
 return 0;
-}
+}// end the main funtion
 
